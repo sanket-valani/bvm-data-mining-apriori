2df4f9afb511f0f9676443506b6f62fa92d8c858
Sanket Valani <sanketvalani@gmail.com>
2020-11-05 14:37:04 +0530

    added css

--- app/views.py
+++ app/views.py
@@ -16,12 +16,11 @@ def result(request):
     import matplotlib.pyplot as plt
     import pandas as pd
 
-    dataset = pd.read_csv('/home/sanket/App/Django-Apriori/app/ml_data/Market_Basket_Optimisation.csv', header = None)
+    dataset = pd.read_csv('/home/sanket/App/Django-Apriori/app/ml_dataset/Market_Basket_Optimisation.csv', header = None)
     transactions = []
     for i in range(0, 7501):
       transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
-    # Training the Apriori model on the dataset
     from apyori import apriori
     rules = apriori(transactions, min_support = min_support, min_confidence = min_conf, min_lift = min_lift, min_length = 2)
     # rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
